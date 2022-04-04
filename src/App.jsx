8ae48b0d21717dc8d777a523947062c30c6031b4
Kossin1337 <piotrswobodzinski@gmail.com>
2022-04-04 14:19:36 +0200

    React Context implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { createContext } from "react";
+
 import { useState } from "react";
 import "./App.scss";
 
@@ -6,17 +8,31 @@ import Clicker from "./components/clicker/Clicker";
 import Sidebar from "./components/sidebar/Sidebar";
 import ActionMenu from "./components/action-menu/ActionMenu";
 
+export const AppContext = createContext();
+
 const App = () => {
-  const [value, setValue] = useState(0);
+  const [currentValue, setCurrentValue] = useState(0);
+  const [onClickIncrement, setOnClickIncrement] = useState(1);
   const [passiveIncomeValue, setPassiveIncomeValue] = useState(0);
 
   return (
-    <div className="app">
-      <Navigation />
-      <Sidebar />
-      <Clicker />
-      <ActionMenu />
-    </div>
+    <AppContext.Provider
+      value={{
+        currentValue,
+        setCurrentValue,
+        onClickIncrement,
+        setOnClickIncrement,
+        passiveIncomeValue,
+        setPassiveIncomeValue,
+      }}
+    >
+      <div className="app">
+        <Navigation />
+        <Sidebar />
+        <Clicker />
+        <ActionMenu />
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- src/components/clicker/Clicker.jsx
+++ src/components/clicker/Clicker.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { AppContext } from "../../App";
 import "./Clicker.scss";
 
 const Clicker = () => {
+  const { currentValue, setCurrentValue, onClickIncrement } =
+    useContext(AppContext);
+
+  /* on first render */
+  // useEffect(() => {}, []);
+
+  /* Update value on button click */
+  const changeValue = () => {
+    console.log(currentValue);
+    setCurrentValue((prevValue) => prevValue + onClickIncrement);
+  };
+
   return (
     <div className="clicker">
-      <h1 className="title">Clicker</h1>
-      <span className="displayed-value">VALUE</span>
-      <button className="clicker-btn">Clicker</button>
+      <h1 className="title">Game Board</h1>
+      <span className="displayed-value">{currentValue}</span>
+      <button className="clicker-btn" onClick={changeValue}>
+        Clicker
+      </button>
     </div>
   );
 };
